283372a4ff7a0792188d9f706d68d62ebaa02414
cidels <66462705+cidels@users.noreply.github.com>
2020-09-10 13:32:35 +0800

    Add files via upload

---
+++ pertemuan07/index.js
@@ -0,0 +1,14 @@
+//if else
+const ip = 3.9;
+iif (time < 10) {
+  greeting = "ohayou gozaimasu";
+} else if (time < 20) {
+  greeting = "konbanwa";
+} else {
+  greeting = "oyasuminasai";
+}
+
+//Looping
+for (let i = 0; i < 5; i++) {
+    console.log("you did great!");
+}
